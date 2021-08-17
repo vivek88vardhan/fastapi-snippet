6f9cb766c907ea0d7c1b8044c711d2078039e247
Girish Mallula <girish.mallula@thinkartha.com>
2021-08-16 18:12:21 -0700

    Base boilerplate

--- src/api/routes/api.py
+++ src/api/routes/api.py
@@ -4,12 +4,10 @@ from fastapi import APIRouter
 # from app.api.routes.articles import api as articles
 
 from src.api.routes.customers import api as customers
-from src.api.routes.authentication import api as authentication
 from src.core.config import API_VERSION
 
 router = APIRouter()
 router.include_router(customers.router, tags=["Customers"], prefix="/"+API_VERSION)
-router.include_router(authentication.router, tags=["Authentication"], prefix="/"+API_VERSION)
 """
 router.include_router(authentication.router, tags=["authentication"], prefix="/users")
 router.include_router(users.router, tags=["users"], prefix="/user")
--- src/api/routes/authentication/api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from src.api.routes.authentication import auth_jwt
-router = APIRouter()
-
-router.include_router(auth_jwt.router, prefix="/auth")
