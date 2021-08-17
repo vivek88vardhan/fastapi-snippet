14332929d6deede2a9a2898d75471f71abf84f60
Girish <chandramgc@gmail.com>
2021-08-17 14:25:08 -0700

    graph update

--- src/api/routes/api.py
+++ src/api/routes/api.py
@@ -4,13 +4,11 @@ from fastapi import APIRouter
 # from app.api.routes.articles import api as articles
 
 from src.api.routes.customers import api as customers
-from src.api.routes.authentication import api as authentication
 from src.api.routes.graphql import api as graphql
 from src.core.config import API_VERSION
 
 router = APIRouter()
 router.include_router(customers.router, tags=["Customers"], prefix="/"+API_VERSION)
-router.include_router(authentication.router, tags=["Authentication"], prefix="/"+API_VERSION)
 router.include_router(graphql.router, tags=["GraphQL"], prefix="/"+API_VERSION)
 
 """
